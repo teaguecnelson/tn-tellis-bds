af1f24418e8934044e4d5084e03d3f16b87e0152
teaguecnelson <teaguecnelson@gmail.com>
2017-08-09 14:21:18 -0400

    recent articles

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -6,5 +6,22 @@
   @include('partials.email-capture')
   @include('partials.three-up-categories')
   @include('partials.authenticity-organism')
-  @include('partials.recent-posts')
+
+  <div class="three-up">
+    <h2 class="three-up-header latest-articles-header">Latest Articles</h2>
+    <div class="latest-articles-section">
+      @if (!have_posts())
+        <div class="alert alert-warning">
+          {{ __('Sorry, no results were found.', 'sage') }}
+        </div>
+        {!! get_search_form(false) !!}
+      @endif
+
+      @while (have_posts()) @php(the_post())
+        @include('partials.content-'.get_post_type())
+      @endwhile
+    </div>
+  </div>
+
+  {!! get_the_posts_navigation() !!}
 @endsection
--- resources/views/partials/recent-posts.blade.php
+++ resources/views/partials/recent-posts.blade.php
@@ -1,11 +1,20 @@
 <h2>Recent Posts</h2>
 <ul>
 <?php
-	$args = array( 'numberposts' => '10' );
+	$args = array(
+    'numberposts' => 5,
+    'offset' => 0,
+    'category' => 0,
+    'orderby' => 'post_date',
+    'order' => 'DESC',
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'suppress_filters' => true );
 	$recent_posts = wp_get_recent_posts( $args );
 	foreach( $recent_posts as $recent ){
-		echo '<li><a href="' . get_permalink($recent["ID"]) . '">'. $recent["post_title"] .'</a>'. $recent["post_excerpt"] .'</li> ';
+		echo '<li><a href="' . get_permalink($recent["ID"]) . '">'. $recent["post_title"] .'</a>'. $recent["the_excerpt()"] .'</li> ';
 	}
 	wp_reset_query();
 ?>
 </ul>
+
