040be0456a627c2875fa38499ef0561760d824f2
teaguecnelson <teaguecnelson@gmail.com>
2017-08-24 12:28:46 -0400

    responsive nav working

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,16 @@
 <header class="site-header">
   @include('partials.nav')
-  <div class="header-inner">
-    <a class="logo-wrapper" href="{{ home_url('/') }}"><img class="logo" src="https://www.backdoorsurvival.com/wp-content/uploads/2016/10/gaye-levy-logo-192px-optimized.png" /></a>
-    <div class="header-right">
-      <div class="header-email">
-        @include('partials.email-input')
+  <div class="header-organism">
+    <div class="header-inner">
+      <a class="logo-wrapper" href="{{ home_url('/') }}"><img class="logo" src="https://www.backdoorsurvival.com/wp-content/uploads/2016/10/gaye-levy-logo-192px-optimized.png" /></a>
+      <div class="header-right">
+        <div class="header-email">
+          @include('partials.email-input')
+        </div>
+        <div class="header-search">
+        <a href="/search-backdoor-survival/"><i class="fa fa-search" aria-hidden="true"></i></a>
+        <div/>
       </div>
-      <div class="header-search">
-      <a href="/search-backdoor-survival/"><i class="fa fa-search" aria-hidden="true"></i></a>
-      <div/>
     </div>
   </div>
 </header>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,24 +1,17 @@
-<!-- <nav class="nav-primary">
+<nav class="topnav" id="myTopnav">
     @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
     @endif
-</nav> -->
-
-<nav id="nav" role="navigation"> <a href="#nav" title="Show navigation">Show navigation</a> <a href="#" title="Hide navigation">Hide navigation</a>
-    <ul class="clearfix">
-        @if (has_nav_menu('primary_navigation'))
-            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-        @endif
-    </ul>
+    <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="navResponsive()">&#9776;</a>
 </nav>
-  
-<script src="http://osvaldas.info/examples/main.js"></script>
 
-<script src="http://osvaldas.info/examples/drop-down-navigation-touch-friendly-and-responsive/doubletaptogo.js"></script> 
-  
 <script>
-    $( function()
-    {
-        $( '#nav li:has(ul)' ).doubleTapToGo();
-    });
+function navResponsive() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 </script>
