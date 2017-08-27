e8e56aa1713b57ff8149805c66e379f7facef22f
teaguecnelson <teaguecnelson@gmail.com>
2017-08-27 12:51:28 -0400

    pages styling

---
+++ resources/views/partials/email-hook.blade.php
@@ -0,0 +1,11 @@
+<div class="email-hook">
+	<div class="email-hook-inner-wrapper">
+		<div class="hook-reward">
+			<img class="efbg-img" src="https://i.pinimg.com/736x/51/6d/ac/516dac82b06d40f392b21160dbf98500--camping-survival-survival-skills.jpg" />
+		</div>
+		<div class="email-hook-form">
+			<h2 class="email-hook-header">Sign Up and Recieve Your Free Copy of the Emergency Food Buyers Guide Ebook</h2>
+			@include('partials.email-capture')
+		</div>
+	</div>
+</div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,17 +4,7 @@
 	        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
 	    @endif
 	</div>
-    <div class="email-nav-capture">
-    	<div class="email-nav-capture-wrapper">
-    		<div class="efbg">
-    			<img class="efbg-img" src="https://i.pinimg.com/736x/51/6d/ac/516dac82b06d40f392b21160dbf98500--camping-survival-survival-skills.jpg" />
-    		</div>
-    		<div class="email-nav-capture-form">
-    			<h2 class="nav-email-header">Sign Up and Recieve Your Free Copy of the Emergency Food Buyers Guide Ebook</h2>
-    			@include('partials.email-capture')
-    		</div>
-    	</div>
-    </div>
+    @include('partials.email-hook')
 </nav>
 
 <script>
