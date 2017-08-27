ec6ae0d015a40cc4670f0cb7c26932fc38fc7c8d
teaguecnelson <teaguecnelson@gmail.com>
2017-08-27 00:54:30 -0400

    email capture in menu

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,20 @@
 <nav class="topnav" id="myTopnav">
-    @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-    @endif
+	<div class="nav-wrapper">
+	    @if (has_nav_menu('primary_navigation'))
+	        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+	    @endif
+	</div>
+    <div class="email-nav-capture">
+    	<div class="email-nav-capture-wrapper">
+    		<div class="efbg">
+    			<img class="efbg-img" src="https://i.pinimg.com/736x/51/6d/ac/516dac82b06d40f392b21160dbf98500--camping-survival-survival-skills.jpg" />
+    		</div>
+    		<div class="email-nav-capture-form">
+    			<h2 class="nav-email-header">Sign Up and Recieve Your Free Copy of the Emergency Food Buyers Guide Ebook</h2>
+    			@include('partials.email-capture')
+    		</div>
+    	</div>
+    </div>
 </nav>
 
 <script>
